18d957535be7139af106682f14db509d766ad6ff
MohanG-25 <84380247+MohanG-25@users.noreply.github.com>
2023-05-19 10:20:21 -0700

    Update Main.js

--- src/components/DisplayPage/Main.js
+++ src/components/DisplayPage/Main.js
@@ -5,7 +5,7 @@ import Train from './Train'
 const Main = ({search}) => {
    const [data,setData]=useState([]);
    useEffect(()=>{
-       axios.get('http://localhost:1000/train')  //getting data from backend server 
+       axios.get('https://train-server-cr9c.onrender.com/train')  //getting data from backend server 
             .then((response)=>setData(response.data));//and storing it in data variable and using it to display
     },[])
    
